00391ca625ab81cc9ffac9cb1a79d509f01b5a93
Davide Icardi <davide.icardi@gmail.com>
2015-02-18 19:26:05 +0000

    added grpu by

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,32 +7,56 @@ angular.module('openweather.controllers', [])
 
 .controller('HomeCtrl', function($scope, $http, $localstorage) {
   
-  var options =
-    $localstorage.getObject("options")
-    || { city : "New York", units: "metric" };
-  $scope.options = options;
+  $scope.doRefresh = function() {
+    var options =
+      $localstorage.getObject("options")
+      || { city : "New York", units: "metric" };
+    $scope.options = options;
+    
+    var url = "http://api.openweathermap.org/data/2.5/forecast"
+    var params = { 
+      q: options.city, 
+      units: options.units 
+    };
+    if (options.appId) {
+      params.APPID = options.appId;
+    }
   
-  var url = "http://api.openweathermap.org/data/2.5/forecast"
-  var params = { 
-    q: options.city, 
-    units: options.units 
+    $http.get(url, { params : params } ).
+      success(function(data, status, headers, config) {
+        
+        $scope.forecast = data;
+        var grouped = {};
+        
+        angular.forEach(data.list, function(dataItem) {
+          dataItem.date = new Date(dataItem.dt * 1000);
+          dataItem.day = new Date(dataItem.dt * 1000);
+          dataItem.day.setHours(0,0,0,0);
+          
+          grouped[dataItem.day] = grouped[dataItem.day] || {items: [], dayDate: dataItem.day};
+          grouped[dataItem.day].items.push(dataItem);
+        });
+        
+        $scope.days = [];
+        angular.forEach(grouped, function(value, key) {
+          $scope.days.push(value);
+        });
+        
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      })
+      .finally(function() {
+         // Stop the ion-refresher from spinning
+         $scope.$broadcast('scroll.refreshComplete');
+       });
   };
-  if (options.appId) {
-    params.APPID = options.appId;
-  }
   
-  $http.get(url, { params : params } ).
-    success(function(data, status, headers, config) {
-      $scope.forecast = data;
-    }).
-    error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-    });
-  
+  $scope.doRefresh();
 })
 
-.controller('OptionsCtrl', function($scope, $log, $localstorage) {
+.controller('OptionsCtrl', function($scope, $log, $localstorage, $state) {
   
   $scope.options =
     $localstorage.getObject("options")
@@ -41,7 +65,8 @@ angular.module('openweather.controllers', [])
   $scope.save = function() {
     
     $localstorage.setObject("options", $scope.options);
-
+    
+    $state.go('app.home', {}, {location: 'replace'});
   };
   
 });
